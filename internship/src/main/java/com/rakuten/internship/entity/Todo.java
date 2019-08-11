058efec556267f90ca8daa08fdbda9a1c1a48563
T45K <tasktas9@gmail.com>
2019-08-11 13:44:49 +0900

    add lombok setter & getter

--- internship/src/main/java/com/rakuten/internship/entity/Todo.java
+++ internship/src/main/java/com/rakuten/internship/entity/Todo.java
@@ -1,5 +1,8 @@
 package com.rakuten.internship.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Todo {
 
     @Id
@@ -19,28 +24,4 @@ public class Todo {
 
     @Column(name = "Description")
     private String description;
-
-    public long getNumber() {
-        return number;
-    }
-
-    public void setNumber(final int number) {
-        this.number = number;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
 }
