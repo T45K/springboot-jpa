5c1e9eac8a827050cdc397717ec2d1e270d800df
T45K <tasktas9@gmail.com>
2019-08-25 23:06:13 +0900

    alert deadline

--- internship/src/main/java/com/rakuten/internship/TodoController.java
+++ internship/src/main/java/com/rakuten/internship/TodoController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.PostConstruct;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * このクラスは、ウェブアプリケーションの挙動を制御するためのコントローラークラスです。。
@@ -44,7 +49,57 @@ public class TodoController {
     public String home(final Model model) {
         final List<Todo> todoList = service.findTodoList();
         model.addAttribute("todoList", todoList);
+
+        final List<Todo> todoToBeAlertedList = getTodoToBeAlerted(todoList);
+        model.addAttribute("todoToBeAlertedList", todoToBeAlertedList);
+
         return "home";
+    }
+
+    private List<Todo> getTodoToBeAlerted(final List<Todo> todoList) {
+        return todoList.stream()
+                .filter(this::isToBeAlerted)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isToBeAlerted(final Todo todo) {
+        if (todo.getDeadline() == null) {
+            return false;
+        }
+
+        final Date today = new Date();
+        final Calendar todayOnCalender = Calendar.getInstance();
+        todayOnCalender.setTime(today);
+
+        final Calendar deadlineOnCalender = convertDeadlineToCalender(parseFormat(todo.getDeadline()));
+
+        return isOneDayBefore(deadlineOnCalender, todayOnCalender);
+    }
+
+    private Date parseFormat(final String dateString) {
+
+        final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        try {
+            return format.parse(dateString);
+        } catch (final ParseException e) {
+            e.printStackTrace();
+            return new Date();
+        }
+    }
+
+    private Calendar convertDeadlineToCalender(final Date date) {
+        final Calendar calender = Calendar.getInstance();
+        calender.setTime(date);
+        return calender;
+    }
+
+    private boolean isOneDayBefore(final Calendar deadline, final Calendar today) {
+        if (deadline.after(today)) {
+            deadline.add(Calendar.DATE, -1);
+            return deadline.before(today);
+        }
+
+        return false;
     }
 
     @GetMapping("/create")
